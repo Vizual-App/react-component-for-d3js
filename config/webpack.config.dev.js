84e17d40d6d12c291712b2f41a52215f392a04b9
vijeyash <91282703+vijeyash1@users.noreply.github.com>
2022-11-19 12:29:39 +0530

    Delete config directory

--- config/webpack.config.dev.js
+++
@@ -1,9 +0,0 @@
-const merge = require('webpack-merge');
-
-module.exports = merge(require('./webpack.config.base'), {
-    mode: 'development',
-    devtool: 'eval',
-    output: {
-        filename: 'react-d3-components.js'
-    }
-});
